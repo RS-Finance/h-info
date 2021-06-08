11c5084b1685291f0fd05137cbae277e5510d7e4
RS Finance <52669170+RS-Finance@users.noreply.github.com>
2021-06-07 22:49:07 -0700

    Add files via upload

---
+++ precache-manifest.73104faaf326a93ae40ae950ba75ff67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9760d44ba8bf979432c44ca3130ab5aa",
+    "url": "/info/index.html"
+  },
+  {
+    "revision": "9bdf6ef0d9fc91829db0",
+    "url": "/info/static/js/2.f8c15435.chunk.js"
+  },
+  {
+    "revision": "571b02884f2fb96a21dccc841ae62912",
+    "url": "/info/static/js/2.f8c15435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b08fded4b6e749b356",
+    "url": "/info/static/js/main.9c7e8146.chunk.js"
+  },
+  {
+    "revision": "a768100733258a46dc76",
+    "url": "/info/static/js/runtime-main.dcb90b7a.js"
+  },
+  {
+    "revision": "350dbfd37fb630b74df90b62a32a983e",
+    "url": "/info/static/media/dfynlogo.350dbfd3.png"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/info/static/media/eth.73dabb37.png"
+  }
+]);
